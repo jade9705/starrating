388e53328916cf7c2b80c9aed076b2ffe1f54af6
unknown <wrannjade@gmail.com>
2020-05-21 08:59:01 +0100

    stars are working

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 'use strict';
 
 const valueNum = document.querySelector('.rating__value');
-
 const valueStars = document.querySelectorAll('.rating__star');
 
+let clickedStarIndex = 3;
 
-
-valueStars.forEach ((star) => {
+valueStars.forEach ((star, index) => {
     star.addEventListener('click', () => {
-        star.classList.toggle('rating__star--on');
-
+        clickedStarIndex = index;
+        // star.classList.toggle('rating__star--on');
+        starLight();
 })})
+
+const starLight =  () => {
+   valueStars.forEach((star, index) => {
+   if (clickedStarIndex >= index) {
+       star.className = "rating__star rating__star--on";
+   } else {
+       star.className = "rating__star";
+
+   }
+
+   })
+
+}
